b8e29b1526fcb220ac2839cc39d5503520af72a4
alyconr <alyconr@hotmail.com>
2023-05-22 17:13:41 -0500

    :fire: javascript code added

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuHamburger = document.querySelector(".hamburguer");
+const navMobile = document.querySelector(".navbar");
+const navMobileLinks = document.querySelectorAll(".nav-list a");
+const hamburgerImage = document.querySelector("#hamburguer");
+
+
