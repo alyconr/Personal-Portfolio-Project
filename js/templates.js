08d8ed6b61676a217e27f3b4df5d41c2a05c1918
alyconr <alyconr@hotmail.com>
2023-05-24 18:53:10 -0500

    :bug: fix Eslint error

--- js/templates.js
+++ js/templates.js
@@ -1,49 +1,49 @@
 const template = (data, index) => `
-<div class=" card   ">
+<div class= 'card '>
     <img
-            src="${data.imagePath}"
-            alt="${data.title}"
-            class="image-card"
+            src='${data.imagePath}'
+            alt='${data.title}'
+            class='image-card'
           />
-          <div class="card-content">
-            <h2 class="tonic ton">${data.title}</h2>
-            <ul class="dates">
-                ${data.dates.map((date) => `<li>${date}</li>`).join("")}
+          <div class='card-content'>
+            <h2 class='tonic ton'>${data.title}</h2>
+            <ul class='dates'>
+                ${data.dates.map((date) => `<li>${date}</li>`).join(' ')}
             </ul>
-            <p class="paragraph">
+            <p class='paragraph'>
               ${data.description}
             </p>
-            <ul class="stacks">
+            <ul class='stacks'>
               ${data.stacks
-                .map((stack) => `<li class="stack-over">${stack}</li>`)
-                .join("")}
+                .map((stack) => `<li class='stack-over'>${stack}</li>`)
+                .join('')}
             </ul>
-            <a href="#" content-id="${index}" class="btn">See Project</a>
+            <a href='#' content-id='${index}' class='btn'>See Project</a>
           </div>
           </div>
     `;
 
 const revTemplate = (data, index) => `
-<div class=" card card-reverse  ">
+<div class=' card card-reverse  '>
     <img
-            src="${data.imagePath}"
-            alt="${data.title}"
-            class="image-card"
+            src='${data.imagePath}'
+            alt='${data.title}'
+            class='image-card'
           />
-          <div class="card-content">
-            <h2 class="tonic ton">${data.title}</h2>
-            <ul class="dates">
-                ${data.dates.map((date) => `<li>${date}</li> `).join("")}
+          <div class='card-content'>
+            <h2 class='tonic ton'>${data.title}</h2>
+            <ul class='dates'>
+                ${data.dates.map((date) => `<li>${date}</li>`).join(' ')}
             </ul>
-            <p class="paragraph">
+            <p class='paragraph'>
               ${data.description}
             </p>
-            <ul class="stacks">
+            <ul class='stacks'>
               ${data.stacks
-                .map((stack) => `<li class="stack-over">${stack}</li>`)
-                .join(" ")}
+                .map((stack) => `<li class='stack-over'>${stack}</li>`)
+                .join(' ')}
             </ul>
-            <a href="#" content-id="${index}" class="btn">See Project</a>
+            <a href='#' content-id='${index}' class='btn'>See Project</a>
           </div>
           </div>
       `;
