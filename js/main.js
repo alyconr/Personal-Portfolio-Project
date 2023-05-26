47dbfc492f755b20faefc2d55d0b219eddf55e05
Julio Eduardo Vergara Martinez <gogczoro@gmail.com>
2023-05-26 16:37:03 -0600

    Add import for preserve-data.js file

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ import contactFormValidation from './contact-form-validation.js';
 import mobileMenu from './menu.js';
 import generateCards from './populatecards.js';
 import popUpWindow from './popupwindow.js';
+import preserveData from './preserve-data.js';
 
 
 mobileMenu();
 generateCards();
 popUpWindow();
 contactFormValidation();
+preserveData();
