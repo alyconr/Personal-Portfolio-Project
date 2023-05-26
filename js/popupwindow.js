b227873cad8387bac33947935262faad4e01eee9
alyconr <alyconr@hotmail.com>
2023-05-26 07:59:50 -0500

    :hammer: Add and updater  feature modal scripts

--- js/popupwindow.js
+++ js/popupwindow.js
@@ -1,33 +1,31 @@
 import cardInfo from "./data.js";
 import {
-   modalWindow
+  modalWindow
 } from "./templates.js";
 
 export default function popUpWindow() {
-   const cardContainer = document.querySelector(".modal-container");
-   cardInfo.forEach((data, i) => {
-      const popup = document.createElement("div");
-      popup.classList.add("open-modal");
-      popup.innerHTML = modalWindow(data, i);
-      cardContainer.appendChild(popup);
-   });
+  const cardContainer = document.querySelector(".modal-container");
+  cardInfo.forEach((data, i) => {
+    const popup = document.createElement("div");
+    popup.classList.add("open-modal");
+    popup.innerHTML = modalWindow(data, i);
+    cardContainer.appendChild(popup);
+  });
 
-   const openModal = document.querySelectorAll(".open-modal");
-   const buttonModal = document.querySelectorAll(".btn-modal");
+  const openModal = document.querySelectorAll(".open-modal");
+  const buttonModal = document.querySelectorAll(".btn-modal");
 
-   buttonModal.forEach((btn, i) => {
-      btn.addEventListener("click", () => {
-         openModal[i].classList.toggle("active");
-      });
-   });
+  buttonModal.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+      openModal[i].classList.toggle("active");
+    });
+  });
 
-   openModal.forEach((modal) => {
-      modal.addEventListener("click", (e) => {
-         if (e.target.classList.contains("open-modal")) {
-            modal.classList.toggle("active");
-         }
-      });
-   });
-
-
+  openModal.forEach((modal) => {
+    modal.addEventListener("click", (e) => {
+      if (e.target.classList.contains("open-modal")) {
+        modal.classList.toggle("active");
+      }
+    });
+  });
 }
