71b562f1c0a18d4a7013d40f06c648be08090836
Julio Eduardo Vergara Martinez <gogczoro@gmail.com>
2023-05-26 12:33:38 -0600

    Fix code position

--- js/popupwindow.js
+++ js/popupwindow.js
@@ -3,36 +3,32 @@ import { modalWindow } from "./templates.js";
 
 export default function popUpWindow() {
   const cardContainer = document.querySelector(".modal-container");
-    cardInfo.forEach((data, i) => {
+  cardInfo.forEach((data, i) => {
     const popup = document.createElement("div");
     popup.classList.add("open-modal");
     popup.innerHTML = modalWindow(data, i);
     cardContainer.appendChild(popup);
-    
+
   });
 
-    const openModal = document.querySelectorAll(".open-modal");
-    const buttonModal = document.querySelectorAll(".btn-modal");
-  
-   
+  const openModal = document.querySelectorAll(".open-modal");
+  const buttonModal = document.querySelectorAll(".btn-modal");
 
+  const overlay = document.querySelector(".overlay");
 
+  buttonModal.forEach((button) => {
+    button.addEventListener("click", () => {
+      const modal = document.querySelector(".open-modal");
+      modal.classList.toggle("active");
+      overlay.classList.toggle("active");
+    });
+  });
+
+  openModal.forEach((modal) => {
+    modal.addEventListener("click", () => {
+      modal.classList.remove("active");
+      overlay.classList.remove("active");
+    });
+  }
+  );
 }
-
-const overlay = document.querySelector(".overlay");
-
-buttonModal.forEach((button) => {
-  button.addEventListener("click", () => {
-    const modal = document.querySelector(".open-modal");
-    modal.classList.toggle("active");
-    overlay.classList.toggle("active");
-  });
-});
-
-openModal.forEach((modal) => {
-  modal.addEventListener("click", () => {
-    modal.classList.remove("active");
-    overlay.classList.remove("active");
-  });
-}
-);
