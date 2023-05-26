0bff38f01e31c75cf5c7f887b410d335e47342bd
Julio Eduardo Vergara Martinez <gogczoro@gmail.com>
2023-05-26 16:30:42 -0600

    Add import for contact form validation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+import contactFormValidation from './contact-form-validation.js';
 import mobileMenu from './menu.js';
 import generateCards from './populatecards.js';
 import popUpWindow from './popupwindow.js';
 
+
 mobileMenu();
 generateCards();
 popUpWindow();
+contactFormValidation();
