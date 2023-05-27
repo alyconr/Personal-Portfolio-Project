b9201a1d6079acd93eaf7afae06273e63636f05d
alyconr <alyconr@hotmail.com>
2023-05-27 14:03:11 -0500

    ::white_check_mark: Add  required chanhes to local storage feature

--- js/main.js
+++ js/main.js
@@ -2,10 +2,15 @@ import mobileMenu from './menu.js';
 import generateCards from './populatecards.js';
 import popUpWindow from './popupwindow.js';
 import validateForm from './contact-form-validation.js';
-import saveFormData from './preserve-data.js';
+import preserveData from './preserve-data.js';
 // Event listener for form submission
 document.querySelector('.form').addEventListener('submit', validateForm);
 mobileMenu();
 generateCards();
 popUpWindow();
-saveFormData();
+preserveData();
+
+
+
+
+
--- js/preserve-data.js
+++ js/preserve-data.js
@@ -1,28 +1,55 @@
-function saveLocalStorage() {
-  const form = document.querySelector('.form');
+// Function to save input data to local storage
 
-  // Add event listener for form submission
-  form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevent the form from submitting
+export default function preserveData() {
+  function saveFormData() {
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const messageInput = document.getElementById('message');
 
-    // Get the input values
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    // Create an object to store the input data
-    const inputData = {
-      name: name,
-      email: email,
-      message: message,
+    // Create an object to store the form data
+    const formData = {
+      name: nameInput.value,
+      email: emailInput.value,
+      message: messageInput.value,
     };
 
-    // Save the input data in local storage
-    localStorage.setItem('formData', JSON.stringify(inputData));
+    // Convert the object to JSON string
+    const formDataJSON = JSON.stringify(formData);
 
-    // Submit the form to the server
-    form.submit();
-  });
+    // Save the JSON string to local storage
+    localStorage.setItem('formData', formDataJSON);
+  }
+
+  // Function to load and pre-fill data from local storage
+  function loadFormData() {
+    const nameInput = document.getElementById('name');
+    const emailInput = document.getElementById('email');
+    const messageInput = document.getElementById('message');
+
+    // Get the saved JSON string from local storage
+    const formDataJSON = localStorage.getItem('formData');
+
+    // Parse the JSON string into an object
+    const formData = JSON.parse(formDataJSON);
+
+    if (formData) {
+      // Pre-fill the input fields with the saved values
+      nameInput.value = formData.name || '';
+      emailInput.value = formData.email || '';
+      messageInput.value = formData.message || '';
+    }
+  }
+
+  // Add event listeners to the input fields
+  const nameInput = document.getElementById('name');
+  nameInput.addEventListener('input', saveFormData);
+
+  const emailInput = document.getElementById('email');
+  emailInput.addEventListener('input', saveFormData);
+
+  const messageInput = document.getElementById('message');
+  messageInput.addEventListener('input', saveFormData);
+
+  // Load the form data when the page is loaded
+  window.addEventListener('load', loadFormData);
 }
-
-export default saveLocalStorage;
