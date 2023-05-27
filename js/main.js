8d912a58d4b9902e41cd6abf3231f5c9c7d9ea12
alyconr <alyconr@hotmail.com>
2023-05-27 12:18:56 -0500

    :sparkles: New feature added so that preserve data in local storage

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import mobileMenu from './menu.js';
 import generateCards from './populatecards.js';
 import popUpWindow from './popupwindow.js';
 import validateForm from './contact-form-validation.js';
-import saveFormData from './localstorage.js';
+import saveFormData from './preserve-data.js';
 // Event listener for form submission
 document.querySelector('.form').addEventListener('submit', validateForm);
 mobileMenu();
